fceec6f0cd142b8d0c22741a530a0ff27f0edf00
datdadev <hackermario54@icloud.com>
2025-04-24 11:31:32 +0000

    auth

--- be/server.js
+++ be/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,15 +15,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 // Routes
 const userRoutes = require("./routes/db_api");
 const minioRoutes = require("./routes/minio_api");
-const paymentRoutes = require("./routes/donate")
+const paymentRoutes = require("./routes/donate");
+const authRoutes = require("./routes/auth");
+
 // Prefix routes
 app.use("/api/user", userRoutes);    
 app.use("/api/minio", minioRoutes);   
 app.use("/api/donate", paymentRoutes);
+app.use("/auth", authRoutes);
 
+// Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
